c30460ec5348b659cf63b7a03b424cc0b4d78455
oumaima-mannai <manaoumaima28@gmail.com>
2025-03-13 09:47:23 +0100

    second modif of project.py

--- project.py
+++ project.py
@@ -1,3 +1,9 @@
 Data [1,3,5]
-Som=sum(Data)
-print('la somme est :',Som)
+#Som=sum(Data)
+#print('la somme est :',Som)
+
+    print('la somme est : ',sum(Data))
+    print('le min est:', min(Data))
+    print('le max est : ', max(Data)
+    print('dossier vide')
+
